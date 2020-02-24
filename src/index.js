9f57de1ce63973524f789503a9fa48607f92ca44
Dev_Dat <hyungmin.b.shim@gmail.com>
2020-02-24 16:20:40 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import Routes from "./Routes";
+import './styles/index.css';
+import './styles/reset.css';
+import './fonts/Spoqa Han Sans Regular.ttf'
+import './fonts/Spoqa Han Sans Bold.ttf'
+import './fonts/Spoqa Han Sans Light.ttf'
+import './fonts/Spoqa Han Sans Thin.ttf'
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
