6840f6108fdd5fecfa0e41c88248bb3b8d6d0926
annkimm <zizicoo208@naver.com>
2020-02-24 16:44:01 +0900

    초기세팅 - component 폴더 추가, Home.js 추가

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import Routes from "./Routes";
-import './styles/index.css';
-import './styles/reset.css';
-import './fonts/Spoqa Han Sans Regular.ttf'
-import './fonts/Spoqa Han Sans Bold.ttf'
-import './fonts/Spoqa Han Sans Light.ttf'
-import './fonts/Spoqa Han Sans Thin.ttf'
+import "./styles/index.css";
+import "./styles/reset.css";
+import "./fonts/Spoqa Han Sans Regular.ttf";
+import "./fonts/Spoqa Han Sans Bold.ttf";
+import "./fonts/Spoqa Han Sans Light.ttf";
+import "./fonts/Spoqa Han Sans Thin.ttf";
+import Home from "./page/Home";
 
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById("root"));
