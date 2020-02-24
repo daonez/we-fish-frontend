45e5a34697abc0b6dc3590eb5ecf129ab947835a
daonez <cyongchoi@gmail.com>
2020-02-24 19:54:46 +0900

    add router

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./page/Home";
+import Product from "./page/product/Products";
 
 class Routes extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/home" component={Home} />
+          <Route exact path="/product" component={Product} />
         </Switch>
       </Router>
     );
