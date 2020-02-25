7db802de07efb3c8c06d559f9d51fad466a930e7
annkimm <zizicoo208@naver.com>
2020-02-25 15:43:45 +0900

    큰 레이아웃 작업

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./Routes";
-import "./styles/index.css";
-import "./styles/reset.css";
 import "./fonts/Spoqa Han Sans Regular.ttf";
 import "./fonts/Spoqa Han Sans Bold.ttf";
 import "./fonts/Spoqa Han Sans Light.ttf";
 import "./fonts/Spoqa Han Sans Thin.ttf";
-import Home from "./page/Home";
+import "./styles/index.scss";
+import Routes from "./Routes";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById("root"));
