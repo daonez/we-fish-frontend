e9e1bdff5be61db960a1cae6940530754131a330
akiakma <akiakma@users.noreply.github.com>
2020-02-26 18:42:14 +0900

    예리님옆에서 수정함3

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,10 @@
+// 이거는 Routes.js
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./page/Home";
+import Product from "./page/product/Product";
+import Login from "./page/Login";
+import SignUp from "./page/SignUp";
 
 class Routes extends React.Component {
   render() {
@@ -8,7 +12,10 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route path="/home" component={Home} />
-
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/product" component={Product} />
         </Switch>
       </Router>
     );
