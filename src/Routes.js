0601ba8aa781bffa8a993090d3d1add294f0e7f5
daonez <cyongchoi@gmail.com>
2020-02-28 15:30:19 +0900

    delete npm

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 // 이거는 Routes.js
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Product from "./pages/product/Product";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Nav from "./pages/Home";
-import MainPage from "./pages/main/Main";
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Home from "./pages/Home"
+import Product from "./pages/product/Product"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Nav from "./pages/Home"
+import MainPage from "./pages/main/Main"
 
 class Routes extends React.Component {
   render() {
@@ -21,8 +21,8 @@ class Routes extends React.Component {
           <Route exact path="/main" component={MainPage} />
         </Switch>
       </Router>
-    );
+    )
   }
 }
 
-export default Routes;
+export default Routes
