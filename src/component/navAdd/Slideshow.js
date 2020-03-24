b9189b9a3b249306aee5fbb1afde882f61c612c3
daonez <cyongchoi@gmail.com>
2020-03-24 22:27:26 +0900

    fixed main slider

--- src/component/navAdd/Slideshow.js
+++ src/component/navAdd/Slideshow.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react"
-import "./Slideshow.scss"
-import { Slide } from "react-slideshow-image"
+import React, { Component } from 'react'
+import './Slideshow.scss'
+import { Slide } from 'react-slideshow-image'
 
 const slideImages = [
-  "https://cdn.onul-hoi.com/uploads/store_banner/image/78/66652b_200224013133.jpg",
-  "https://cdn.onul-hoi.com/uploads/store_banner/image/88/c092a3_200224012803.jpg",
-  "https://cdn.onul-hoi.com/uploads/store_banner/image/83/1e1f9a_200224012844.jpg"
+  'https://cdn.onul-hoi.com/uploads/store_banner/image/83/be3f0d_200323060144.jpg',
+  'https://cdn.onul-hoi.com/uploads/store_banner/image/89/4b0097_200322150231.jpg',
+  'https://cdn.onul-hoi.com/uploads/store_banner/image/78/d35c50_200315145630.jpg',
+  'https://cdn.onul-hoi.com/uploads/store_banner/image/88/2bf6f6_200322150158.jpg',
+  'https://cdn.onul-hoi.com/uploads/store_banner/image/86/628e2e_200323062310.png',
 ]
 
 const properties = {
@@ -16,7 +18,7 @@ const properties = {
   arrows: true,
   onChange: (oldIndex, newIndex) => {
     console.log(`slide transition from ${oldIndex} to ${newIndex}`)
-  }
+  },
 }
 
 const Slideshow = () => {
@@ -32,11 +34,15 @@ const Slideshow = () => {
         <div className="each-slide">
           <div style={{ backgroundImage: `url(${slideImages[2]})` }} />
         </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[3]})` }} />
+        </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[4]})` }} />
+        </div>
       </Slide>
     </div>
   )
 }
 
-export default Slideshow;
-
-
+export default Slideshow
